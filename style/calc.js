9527300f83ce2651b70a8caaf45391029c324b47
sftn0928 <kobayashi19990928@gmail.com>
2020-07-07 15:26:32 +0900

    小数点修正

--- style/calc.js
+++ style/calc.js
@@ -1,13 +1,14 @@
 var lastSym = "";
 var preNum = 0;
 var result = 0;
+var isInteger = true;
 
 // 数字を押したときのイベント
-clickNumber = (num) => {
+const clickNumber = (num) => {
   // 現在の値を代入
   var currentNum = document.getElementById("currentNum").value;
   if (currentNum === "0" || currentNum === preNum ||
-    result === parseInt(currentNum) && isPoint ) {
+    result === parseInt(currentNum) && isInteger ) {
     currentNum = num;
   } else {
     if (num === '0') {
@@ -20,7 +21,7 @@ clickNumber = (num) => {
 };
 
 // 記号を押したときのイベント
-clickSymbol = (sym) => {
+const clickSymbol = (sym) => {
   if (sym != "=") {
     if (sym === "AC") allClear();
     if (sym === "C") Clear();
@@ -56,25 +57,25 @@ clickSymbol = (sym) => {
 };
 
 // .を押した時のイベント
-clickPoint = () => {
+const clickPoint = () => {
   let pointNum = document.getElementById("currentNum").value;
   // 整数判定の正規表現
   let patten = /^[+,-]?([1-9]\d*|0)$/;
-  let isPoint = patten.test(pointNum)
+  isInteger = patten.test(pointNum)
   //　現在値が正数の時のみ小数点付与
-  if (isPoint) {
+  if (isInteger) {
     pointNum += ".";
   } 
   document.getElementById("currentNum").value = pointNum;
 }
 
-// ACを押した時のメソッド
-allClear = () => {
+// ACを押した時の関数
+const allClear = () => {
   document.getElementById("currentNum").value = 0;
 }
 
-// Cを押した時のメソッド
-Clear = () => {
+// Cを押した時の関数
+const Clear = () => {
   currentNum = document.getElementById("currentNum").value;
   let divNum = currentNum / 10;
   if (Math.abs(divNum) > 0) {
@@ -84,8 +85,8 @@ Clear = () => {
   }
 }
 
-// +/-を押した時のメソッド
-switchPlusMinus = () => {
+// +/-を押した時の関数
+const switchPlusMinus = () => {
   let absNum = document.getElementById("currentNum").value;
   if (absNum > 0) {
     absNum = "-" + absNum
